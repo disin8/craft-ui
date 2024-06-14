b72712b42323462a21ea627d02f16f235d6b949a
disin8 <dmitry.sin8@gmail.com>
2024-06-14 14:31:25 +0300

    feat: :rocket: v0.1.0

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -1,36 +1,21 @@
 import Link from 'next/link'
-import { ChevronRight } from 'lucide-react'
 import { Suspense } from 'react'
 import { StarsBadge, StarsBadgeFallback } from './stars'
 import { cn } from '@/lib/utils'
-import { Badge } from '@/components/ui/badge'
 import { Button } from '@/components/ui/button'
 
 export function Hero() {
   return (
     <div className="my-10 flex w-full flex-col justify-center gap-1 px-3 py-4 text-center md:my-20 md:p-6">
-      <div>
-        <Badge variant="outline" className="backdrop-blur-[2px]">
-          <Link
-            href="/components"
-            target="_blank"
-            rel="noreferrer"
-            className="flex items-center"
-          >
-            beatiful components
-            <ChevronRight className="ml-1 h-3 w-3" />
-          </Link>
-        </Badge>
-      </div>
       <div className="flex flex-col gap-6">
         <h1
           className={cn(
-            'text-foreground font-cal text-2xl md:text-4xl md:leading-tight mt-8',
+            'text-foreground font-cal text-5xl md:leading-tight mt-8',
           )}
         >
-          Elevate your React projects with
+          craft
           {' '}
-          <span className="block text-7xl font-bold gradient-text leading-normal">Craft UI</span>
+          <span className="text-7xl font-bold gradient-text leading-normal">UI</span>
         </h1>
         <p className="text-muted-foreground mx-auto max-w-md text-lg md:max-w-xl md:text-xl ">
           A comprehensive component library that seamlessly integrates Tailwind CSS and Framer Motion.
